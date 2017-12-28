c0db57b44c7fb4261a456a57ef683ea753cf97b8
ginniehench_dsk_tp <ginniehench@gmail.com>
2017-12-28 10:07:56 -0500

    Learn how to write tests for routes using login and logout paths

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test 'login get path is generated and recognized' do
+    assert_routing '/login', controller: 'sessions', action: 'new'
+  end
+
+  test 'login post path is generated and recognized' do
+    assert_routing({path: 'login', method: :post}, {controller: 'sessions', action: 'create'})
+  end
+
+  test 'logout path is generated and recognized' do
+    assert_routing({method: 'delete', path: 'logout'}, {controller: "sessions", action: "destroy"})
+  end
+
   test "should get new" do
     get login_path
     assert_response :success
   end
-
 end
