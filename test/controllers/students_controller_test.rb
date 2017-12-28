a20dabe0fde8165180db44aff76c2695cc210489
ginniehench_dsk_tp <ginniehench@gmail.com>
2017-12-28 11:47:01 -0500

    Working on writing integration tests
    Given current state; 2 tests in students_controller FAIL with the following message:
    Expected response to be a <2XX: success>, but was a <302: Found> redirect to <http://www.example.com/login>
    Response body: <html><body>You are being <a href=http://www.example.com/login>redirected</a>.</body></html>
    
    The test for redirecting a student_user with no credentals passes, but its a shallow test.
    
    Keep working on how to test authentication

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,17 +1,24 @@
 require 'test_helper'
 
 class StudentsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @stud_no_creds = students(:stud_u_no_creds)
+    @stud_plus_creds = students(:stud_u_creds)
+  end
+
   test "should get index" do
     # skip
     get students_url
     assert_response :success
   end
 
-  test "should show student" do
-    # skip
-    student = students(:one)
-    get student_url(student)
-    assert_response :success
+  test "should redirect unauthorized student to login" do
+    get student_path(@stud_no_creds.id)
+    assert_redirected_to login_url
   end
 
+  test "should allow authorized student to access show route" do
+    get student_path(@stud_plus_creds.id)
+    assert_response :success
+  end
 end
