b77d21561f74e752d0644d44a14a58085a520206
ginniehench_dsk_tp <ginniehench@gmail.com>
2017-12-30 20:37:31 -0500

    The tests for the Grades Controller are not working the way I need them to. Leave for now and move on

--- test/controllers/grades_controller_test.rb
+++ test/controllers/grades_controller_test.rb
@@ -1,34 +1,35 @@
 require 'test_helper'
 
 class GradesControllerTest < ActionDispatch::IntegrationTest
-  # def setup
-  #   @physics = courses(:physics)
-  #   @stud_w_3_cs = students(:three)
-  # end
-  #
-  # test 'when teacher adds eligible student to course, a new grade is stored' do
-  #   get new_course_grade_path(@physics.id)
-  #   assert_response :success
-  #   post new_course_grade, params: {
-  #     students_name: "Ron Weasley"
-  #   }
-  #
-  # end
-  #
-  # test 'downcase input from user can be used to query db' do
-  # end
-  #
-  # test "login with valid information allows access" do
-  #   get login_path
-  #   post login_path, params: {
-  #     username: @admin_u_auth.username,
-  #     password: "password"
-  #     }
-  #   assert is_logged_in?
-  #   assert_redirected_to courses_path
-  #   follow_redirect!
-  #   assert_select "a[href=?]", login_path, count: 0
-  #   assert_select "a[href=?]", logout_path
-  #   assert_select 'p', "Welcome, #{@admin_u_auth.get_proper_name} | Logout"
-  # end
+  def setup
+    @physics = courses(:physics)
+    @stud_w_3_cs = students(:three)
+    @physics_teach_user = users(:teach_use_3)
+  end
+
+  test 'when teacher adds eligible student to course, a new grade is stored' do
+    skip
+    log_in_as(@physics_teach_user)
+    assert is_logged_in?
+    c_id = @physics.id
+    get new_course_grade_path (c_id)
+    assert_response :success
+    post "/courses/" + c_id.to_s + "/grades",
+      params: { students_name: "Ron Weasley" }
+    assert_redirected_to course_path (c_id)
+    follow_redirect!
+  end
+
+  test 'downcase input from user can be used to query db' do
+    skip
+    log_in_as(@physics_teach_user)
+    assert is_logged_in?
+    c_id = @physics.id
+    get new_course_grade_path (c_id)
+    assert_response :success
+    post "/courses/" + c_id.to_s + "/grades",
+      params: { students_name: "ronweasley" }
+    assert_redirected_to course_path (c_id)
+    follow_redirect!
+  end
 end
